9146a5647c6c58debc3c0b1eff6edf1a1dd36e21
Sartaj <sartaj@sartaj.me>
2017-09-09 00:03:38 -0500

    remove create

--- packages/create/README.md
+++
@@ -1,6 +0,0 @@
-
-# create-elm-in-js-app
-
-Create [elm](https://elm-lang.org/) flavored JS apps with no build configuration.
-
-[Learn more](https://github.com/sartaj/elm-in-js).
--- packages/create/template/src/typescript/app.test.ts
+++
@@ -1,8 +0,0 @@
-module.exports = replacements => `// import assert from 'assert'
-
-// describe('App', function () {
-//   it('should test something', function () {
-//     // TODO: Add your tests here
-//   })
-// })
-`
