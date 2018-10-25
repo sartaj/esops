73ef51a613e9e5e06f2429e1389f350ef2e1117d
Sartaj <sartaj@sartaj.me>
2018-10-25 01:27:41 -0500

    cleanup features

--- modules/esops/source/features/index.js
+++ modules/esops/source/features/index.js
@@ -1,3 +1,6 @@
+function concatPackageJson() {}
+function mapPropsToTemplate() {}
+function readEsopsTemplateModule() {}
 function updateGitIgnore(paths) {
   const startLine = "\n### ESOPS AUTO GENERATED BEGIN ###\n";
   const endLine = "\n### ESOPS AUTO GENERATED END ###\n";
