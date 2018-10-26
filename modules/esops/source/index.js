1cc74542676e13d49eb3547db7de2a348bf8e770
Sartaj <sartaj@sartaj.me>
2018-10-26 14:55:51 -0500

    update mocks

--- modules/esops/source/index.js
+++ modules/esops/source/index.js
@@ -1,6 +1,7 @@
 const fs = require("fs-plus");
+const jetpack = require("fs-jetpack");
 const path = require("path");
-
+const isDirectory = require("is-directory");
 const capabilities = {
   updateGeneratedTextFs: require("update-generated-text-fs")
   // copyTemplateFiles
@@ -33,4 +34,68 @@ const getInfrastructurePath = infrastructurePath => {
   return paths;
 };
 
+const renderProps = (input, props) => {
+  if (path.extname(input) === "template") renderTemplate(input, props);
+  if (path.basename(input) === "package.json") renderPackageJson(output);
+};
+
+const renderFile = (input, inputRoot, output, props) => {
+  const outputFile = output + input.replace(inputRoot, "");
+  const renderedFile = renderTempFile(input, props);
+  jetpack.copy(renderedFile, output, { overwrite: true });
+};
+
+const renderPaths = (inputs, inputRoot, output, props) => {
+  inputs.forEach(input => {
+    renderFile(input, inputRoot, output, props);
+  });
+};
+
+const render = (input, output, props) => {
+  const inputs = fs.listTreeSync(input);
+  renderPaths(inputs, input, output, props);
+};
+
+const parseStack = stackPath => {};
+
+// {
+//   inputFile: ''
+//   inputRoot: '',
+//   outputFile: '',
+//   props: {},
+// }
+
+const getListFromManifest = (cwd, pkg, props) => {
+  const resolvePkg = require("resolve-pkg");
+  const stackDirectory = resolvePkg(pkg, { cwd });
+};
+
+const createGenerationManifest = (cwd, esopsManifest) => {
+  let generationManifest = [];
+  esopsManifest.forEach(stack => {
+    let results = [];
+    if (typeof stack === "string") {
+      results = getListFromManifest(cwd, stack);
+    } else if (Array.isArray(stack)) {
+      results = getListFromManifest(cwd, stack[0], stack[1]);
+    } else {
+      throw new Error("Error parsing manifest");
+    }
+    generationManifest = generationManifest.concat(results);
+  });
+  return generationManifest;
+};
+
+const run = (cwd, manifestOpts) => {
+  const infrastructureList = createGenerationManifest(cwd, manifestOpts);
+  features.checkForDuplicatePaths(filesManifest);
+};
+
+const runBin = () => {
+  const cwd = process.cwd();
+  const manifestOpts = require(path.join(cwd, "esops.json"));
+  run(cwd, manifestOpts);
+};
+
 module.exports.getInfrastructurePath = getInfrastructurePath;
+module.exports.render = render;
--- modules/esops/specifications/index.js
+++ modules/esops/specifications/index.js
@@ -1,21 +1,52 @@
 const test = require("tape");
 const fs = require("fs-plus");
 const path = require("path");
+const rimraf = require("rimraf");
 
 const esops = require("../source");
 
-test("test basic", t => {
+const MOCK_INFRASTRUCTURES = {
+  basic: path.join(__dirname, "mocks/infrastructures", "basic"),
+  "pipe-me": path.join(__dirname, "mocks/infrastructures", "pipe-me"),
+  "target-web": path.join(__dirname, "mocks/infrastructures", "target-web")
+};
+
+withTempFolder = callback => t => {
+  const dirname = __dirname + "/.tmp/";
+  if (!fs.existsSync(dirname)) fs.mkdirSync(dirname);
+  callback({ t, dirname });
+  rimraf.sync(dirname, fs);
+};
+
+// test('read infrastructure jsx interface')
+
+// test('convert jsx to infrastructure opts')
+
+test("get list of paths from infrastructure directory", t => {
   t.plan(1);
-  const mockDir1 = path.join(__dirname, "mocks", "basic");
-  const actual = esops.getInfrastructurePath(mockDir1);
-  const expected = [__dirname + "/mocks/basic/foo.md"];
+  const infrastructureDirectory = MOCK_INFRASTRUCTURES.basic;
+  const actual = esops.getInfrastructurePath(infrastructureDirectory);
+  const expected = [
+    path.join(infrastructureDirectory, ".vscode"),
+    path.join(infrastructureDirectory, ".vscode/settings.json"),
+    path.join(infrastructureDirectory, "src"),
+    path.join(infrastructureDirectory, "src/stores"),
+    path.join(infrastructureDirectory, "src/stores/stores-architecture.md"),
+    path.join(infrastructureDirectory, "tsconfig.json")
+  ];
   t.deepEqual(actual, expected);
 });
 
-// test("test render", t => {
-//   t.plan(1);
-//   const mockDir1 = path.join(__dirname, "mocks", "basic");
-//   const actual = esops.render(mockDir1);
-//   const expected = [__dirname + "/mocks/basic/foo.md"];
-//   t.deepEqual(actual, expected);
-// });
+// test('compose infrastructures')
+
+test(
+  "test render",
+  withTempFolder(({ t, dirname }) => {
+    t.plan(1);
+    const infrastructureDirectory = MOCK_INFRASTRUCTURES.basic;
+    fs.copySync(infrastructureDirectory, dirname);
+    const props = {};
+    // const actual = esops.render(dirname, infrastructureDirectory, props);
+    t.equal(true, true);
+  })
+);
