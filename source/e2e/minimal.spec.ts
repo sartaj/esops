6600440a8eea33a274df2d65b3109f34d5021ad6
Sartaj <sartaj@sartaj.me>
2018-12-24 03:13:52 -0600

    update snaps to e2e

--- source/e2e/minimal.spec.ts
+++ source/e2e/minimal.spec.ts
@@ -11,90 +11,82 @@ import {
   cleanErrorString
 } from '../../test-utils/fs-utils'
 
-import esops from './index'
+import esops from '../run/index'
 import {MOCK_STACKS} from '../core/examples'
 
-const describe = withSnapshots(__dirname, null)
+const describe = withSnapshots(__dirname, 'snapshots-minimal')
 
-describe('minimal stack with esops.json and fs path', async (assert, assertSnap) => {
-  withTempDir(__dirname, MOCK_STACKS['basic-gitignore'], async cwd => {
+describe('esops() minimal features', async (assert, assertSnap) => {
+  await withTempDir(__dirname, MOCK_STACKS['basic-gitignore'], async cwd => {
     await esops({cwd})
 
-    assertSnap({
-      given: 'a minimal package',
+    await assertSnap({
+      given: 'stack with esops.json and fs path',
       should: 'generate basic template in cwd',
       snap: getSortedFilePaths(cwd)
     })
 
-    assertSnap({
+    await assertSnap({
       given: 'an included gitignore',
       should: 'have updated gitignore with generated file paths',
       snap: getFileContents(path.join(cwd, '.gitignore'))
     })
   })
-})
 
-describe('minimal stack with package.json', async (assert, assertSnap) => {
-  withTempDir(__dirname, MOCK_STACKS['basic-package-json'], async cwd => {
+  await withTempDir(__dirname, MOCK_STACKS['basic-package-json'], async cwd => {
     await esops({cwd})
 
-    assertSnap({
+    await assertSnap({
       given: 'a minimal package with package.json',
       should: 'generate basic template in cwd',
       snap: getSortedFilePaths(cwd)
     })
 
-    assertSnap({
-      given: 'an included .gitignore',
-      should: 'have updated .gitignore with generated file paths',
+    await assertSnap({
+      given: 'a minimal package.json and included .gitignore',
+      should: 'have updated .gitignore with generated paths',
       snap: getFileContents(path.join(cwd, '.gitignore'))
     })
 
-    assertSnap({
-      given: 'a package.json',
-      should: 'have a package.json',
+    await assertSnap({
+      given: 'a minimal package.json',
+      should: 'have contents in package.json',
       snap: getJsonContents(path.join(cwd, 'package.json'))
     })
   })
-})
 
-describe('minimal stack with no .gitignore', async (assert, assertSnap) => {
-  withTempDir(__dirname, MOCK_STACKS['basic'], async cwd => {
+  await withTempDir(__dirname, MOCK_STACKS['basic'], async cwd => {
     await esops({cwd})
 
-    assertSnap({
+    await assertSnap({
       given: 'a minimal package with no .gitignore',
       should: 'generate basic template in cwd',
       snap: getSortedFilePaths(cwd)
     })
 
-    assert({
+    await assert({
       given: 'no .gitignore',
       should: 'not create a gitignore',
       expected: null,
       actual: getFileContents(path.join(cwd, '.gitignore'))
     })
   })
-})
 
-describe('minimal stack from node module', async (assert, assertSnap) => {
   await withTempDir(__dirname, MOCK_STACKS['basic-node-module'], async cwd => {
     await spawn(`npm`, ['install'], {cwd})
     await esops({cwd})
     await assertSnap({
-      given: 'a minimal package',
+      given: 'minimal stack from node module',
       should: 'generate basic template in cwd from node_module',
       snap: getSortedFilePaths(cwd)
     })
   })
-})
 
-describe('an invalid config', async (assert, assertSnap) => {
-  withTempDir(__dirname, MOCK_STACKS['basic-bad-path'], async cwd => {
+  await withTempDir(__dirname, MOCK_STACKS['basic-bad-path'], async cwd => {
     const cleanError = cleanErrorString(cwd)
     await assertSnap({
-      given: 'a bad config',
-      should: 'throw friendly error',
+      given: 'a bad path in the config',
+      should: 'throw a friendly error',
       snap: cleanError(await Try(esops, {cwd}))
     })
   })
